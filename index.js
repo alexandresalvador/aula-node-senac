c9a568255adee23df00ebb5d7851affc09486947
alexandresalvador <alexandre.salvador01@gmail.com>
2023-08-17 21:44:38 -0300

    feat add 3

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 import express from "express"
+import { somar, subtrair } from "./exercicios/todos.js";
 
-const app = express();
+const app = express(); 
+app.use(express.json());
+
 const port = 3000
 
-app.get("/api/:userId/pessoa/:personId", (req,res) => {
-    const nome = req.query.nome
-    const userId = req.params.userId
-    const personId = req.params.personId
+app.get("/api/exercicio1", (req,res) => {
+    const num10 = parseFloat(req.query.num1);
+    const num20 = parseFloat(req.query.num2);
 
     res.json({ 
         message: 
-        `Voce passou o queryParam : ${nome}, `+
-        `routeParam userId: ${userId}, routeParam personId: ${personId}`,
+        `Numero 1 : ${num1}, Numero 2 : ${num2}`,
+    });
+});
+
+app.post("/api/exum", (req,res) => {
+
+    const result = somar();
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+
+    res.json({ 
+        message: 
+        `Resultado : ${num1 + num2}`,
+    });
+});
+
+app.post("/api/exdois", (req,res) => {
+
+    const result = somar();
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+
+    res.json({ 
+        message: 
+        `Resultado : ${num1 + num2}`,
     });
 });
 
